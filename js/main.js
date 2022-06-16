63f52c5467800c37a14bba73bb0674ad2637c98e
Riyadh Hussain <ci.riyadh@gmail.com>
2022-06-16 23:37:04 +0600

    init

---
+++ js/main.js
@@ -0,0 +1,467 @@
+const QUOTE = [
+"Hardship often prepares an ordinary person for an extraordinary destiny.",
+"Christopher Markus",
+
+"There is no easy way from the earth to the stars.",
+"Seneca",
+
+"Well, I must endure the presence of a few caterpillars if I wish to become acquainted with the butterflies.",
+"Antoine de Saint-Exupéry",
+
+"Every man is a creature of the age in which he lives and few are able to raise themselves above the ideas of the time.",
+"Voltaire",
+
+"You have power over your mind – not outside events. Realize this, and you will find strength.",
+"Marcus Aurelius",
+
+"Life is never made unbearable by circumstances, but only by lack of meaning and purpose.",
+"Victor Frankl",
+
+"The highest activity a human being can attain is learning for understanding, because to understand is to be free.",
+"Baruch Spinoza",
+
+"Be like water making its way through cracks. Do not be assertive, but adjust to the object, and you shall find a way around or through it. If nothing within you stays rigid, outward things will disclose themselves. Empty your mind, be formless. Shapeless, like water. If you put water into a cup, it becomes the cup. You put water into a bottle and it becomes the bottle. You put it in a teapot, it becomes the teapot. Now, water can flow or it can crash. Be water, my friend.",
+"Bruce Lee",
+
+"I started my life with a single absolute: that the world was mine to shape in the image of my highest values and never to be given up to a lesser standard, no matter how long or hard the struggle.",
+"Ayn Rand",
+
+"The most beautiful experience we can have is the mysterious. It is the fundamental emotion that stands at the cradle of true art and true science.",
+"Albert Einstein",
+
+"Life must be understood backward. But it must be lived forward .",
+"Søren Kierkegaard",
+
+"Man is a mystery. It needs to be unraveled, and if you spend your whole life unraveling it, don’t say that you’ve wasted time. I am studying that mystery because I want to be a human being.",
+"Fyodor Dostoevsky",
+
+"The flame that burns Twice as bright burns half as long.",
+"Lao Tzu",
+
+"Dwell on the beauty of life. Watch the stars, and see yourself running with them.",
+"Marcus Aurelius",
+
+"Those who educate children well are more to be honored than they who produce them; for these only gave them life, those the art of living well.",
+"Aristotle",
+
+"The price good men pay for indifference to public affairs is to be ruled by evil men.",
+"Plato",
+
+"It is easy in the world to live after the world’s opinion; it is easy in solitude to live after our own; but the great man is he who in the midst of the crowd keeps with perfect sweetness the independence of solitude.",
+"Ralph Waldo Emerson",
+
+"It is one thing to show a man that he is in error, and another to put him in possession of truth.",
+"John Locke",
+
+"The only thing I know is that I know nothing.",
+"Socrates",
+
+"Never let your sense of morals prevent you from doing what is right.",
+"Isaac Asimov",
+
+"The happiness of your life depends upon the quality of your thoughts.",
+"Marcus Aurelius",
+
+"Freedom is secured not by the fulfilling of one’s desires, but by the removal of desire.",
+"Epictetus",
+
+"Everything we hear is an opinion, not a fact. Everything we see is a perspective, not the truth.",
+"Marcus Aurelius",
+
+"The greater the difficulty, the more glory in surmounting it.",
+"Epicurus",
+
+"The function of prayer is not to influence God, but rather to change the nature of the one who prays.",
+"Søren Kierkegaard",
+
+"Happiness is the highest good.",
+"Aristotle",
+
+"The brave man is he who overcomes not only his enemies but his pleasures.",
+"Democritus",
+
+"Without music, life would be a mistake.",
+"Friedrich Nietzsche",
+
+"Never doubt that a small group of thoughtful, committed, citizens can change the world. Indeed, it is the only thing that ever has.",
+"Margaret Mead",
+
+"One, remember to look up at the stars and not down at your feet. Two, never give up work. Work gives you meaning and purpose and life is empty without it. Three, if you are lucky enough to find love, remember it is there and don’t throw it away.",
+"Stephen Hawking",
+
+"Those who know do not speak. Those who speak do not know.",
+"Lao Tsu",
+
+"When you arise in the morning think of what a privilege it is to be alive, to think, to enjoy, to love …",
+"Marcus Aurelius",
+
+"Liberty consists in doing what one desires.",
+"John Stuart Mill",
+
+"Wise men speak because they have something to say; fools because they have to say something.",
+"Plato",
+
+"Whereof one cannot speak, thereof one must be silent.",
+"Ludwig Wittgenstein",
+
+"Good and evil, reward and punishment, are the only motives to a rational creature.",
+"John Locke",
+
+"I was within and without, simultaneously enchanted and repelled by the inexhaustible variety of life.",
+"Fitzgerald F. Scott",
+
+"All that is necessary for the triumph of evil is that good men do nothing.",
+"Edmund Burke",
+
+"The reasonable man adapts himself to the world: the unreasonable one persists in trying to adapt the world to himself. Therefore all progress depends on the unreasonable man.",
+"George Bernard Shaw",
+
+"We are what we repeatedly do. Excellence, then, is not an act, but a habit.",
+"Aristotle",
+
+"Beats all the lies you can invent.",
+"William Blake",
+
+"We are too weak to discover the truth by reason alone.",
+"St. Augustine",
+
+"Maybe everyone can live beyond what they’re capable of.",
+"Markus Zusak",
+
+"Happiness lies in virtuous activity, and perfect happiness lies in the best activity, which is contemplative.",
+"Aristotle",
+
+"I cannot teach anybody anything. I can only make them think",
+"Socrates",
+
+"Morality is not the doctrine of how we may make ourselves happy, but of how we may make ourselves worthy of happiness.",
+"Immanuel Kant",
+
+"We are addicted to our thoughts. We cannot change anything if we cannot change our thinking.",
+"Santosh Kalwar",
+
+"It is wrong always, everywhere and for everyone, to believe anything upon insufficient evidence.",
+"W. K. Clifford",
+
+"To choose doubt as a philosophy of life is akin to choosing immobility as a means of transportation.",
+"Yann Martel",
+
+"I don’t know why we are here, but I’m pretty sure it is not in order to enjoy ourselves.",
+"Ludwig Wittgenstein",
+
+"A concept is a brick. It can be used to build a courthouse of reason. Or it can be thrown through the window.",
+"Gilles Deleuze",
+
+"One cannot conceive anything so strange and so implausible that it has not already been said by one philosopher or another.",
+"René Descartes",
+
+"Life is really simple, but we insist on making it complicated.",
+"Confucius",
+
+"Even while they teach, men learn.",
+"Seneca",
+
+"Love is the extremely difficult realization that something other than oneself is real.",
+"Iris Murdoch",
+
+"Leisure is the mother of philosophy.",
+"Thomas Hobbes",
+
+"No! Try not. Do, or do not. There is no try.",
+"George Lucas",
+
+"Time is a game played beautifully by children.",
+"Heraclitus",
+
+"The mind is furnished with ideas by experience alone.",
+"John Locke",
+
+"This is my simple religion. No need for temples. No need for complicated philosophy. Your own mind, your own heart is the temple. Your philosophy is simple kindness.",
+"Dalai Lama XIV",
+
+"Man is condemned to be free.",
+"Jean-Paul Sartre",
+
+"The journey is what brings us happiness not the destination.",
+"Dan Millman",
+
+"All is for the best in the best of all possible worlds.",
+"Voltaire",
+
+"It is not death that a man should fear, but he should fear never beginning to live.",
+"Marcus Aurelius",
+
+"I would never die for my beliefs because I might be wrong.",
+"Bertrand Russell",
+
+"The aim of art is to represent not the outward appearance of things, but their inward significance.",
+"Aristotle",
+
+"He who thinks great thoughts, often makes great errors.",
+"Martin Heidegger",
+
+"I don’t know why we are here, but I’m pretty sure that it is not in order to enjoy ourselves.",
+"Ludwig Wittgenstein",
+
+"A little philosophy inclineth man’s mind to atheism; but depth in philosophy bringeth men’s minds about to religion.",
+"Sir Francis Bacon",
+
+"Devotion to the truth is the hallmark of morality; there is no greater, nobler, more heroic form of devotion than the act of a man who assumes the responsibility of thinking.",
+"Ayn Rand",
+
+"Things alter for the worse spontaneously, if they be not altered for the better designedly.",
+"Francis Bacon",
+
+"To do as one would be done by, and to love one’s neighbor as oneself, constitute the ideal perfection of utilitarian morality.",
+"John Stuart Mill",
+
+"Whatever is reasonable is true, and whatever is true is reasonable.",
+"G. W. F. Hegel",
+
+"This is patently absurd; but whoever wishes to become a philosopher must learn not to be frightened by absurdities.",
+"Bertrand Russell",
+
+"Our life is what our thoughts make it.",
+"Marcus Aurelius",
+
+"You can discover more about a person in an hour of play than in a year of conversation.",
+"Plato",
+
+"Science is organized knowledge. Wisdom is organized life.",
+"Will Durant",
+
+"There is only one good, knowledge, and one evil, ignorance.",
+"Socrates",
+
+"Don’t only practice your art, but force your way into its secrets, for it and knowledge can raise men to the divine.",
+"Ludwig van Beethoven",
+
+"That man is wisest who, like Socrates, realizes that his wisdom is worthless.",
+"Plato",
+
+"The seed of suffering in you may be strong, but don’t wait until you have no more suffering before allowing yourself to be happy.",
+"Thich Nhat Hanh",
+
+"History is Philosophy teaching by examples.",
+"Thucydides",
+
+"It may be important to great thinkers to examine the world, to explain and despise it. But I think it is only important to love the world, not to despise it, not for us to hate each other, but to be able to regard the world and ourselves and all beings with love, admiration, and respect.",
+"Hermann Hesse",
+
+"If you would be a real seeker after truth, it is necessary that at least once in your life you doubt, as far as possible, all things.",
+"René Descartes",
+
+"Learning does not make one learned: there are those who have knowledge and those who have understanding. The first requires memory and the second philosophy.",
+"Alexandre Dumas",
+
+"I can control my passions and emotions if I can understand their nature.",
+"Spinoza",
+
+"Don’t just say you have read books. Show that through them you have learned to think better, to be a more discriminating and reflective person. Books are the training weights of the mind. They are very helpful, but it would be a bad mistake to suppose that one has made progress simply by having internalized their contents.",
+"Epictetus",
+
+"If men were born free, they would, so long as they remained free, form no conception of good and evil.",
+"Baruch Spinoza",
+
+"It is the power of the mind to be unconquerable.",
+"Seneca",
+
+"He who is unable to live in society, or who has no need because he is sufficient for himself, must be either a beast or a god.",
+"Aristotle",
+
+"Even the finest sword plunged into salt water will eventually rust.",
+"Sun Tzu",
+
+"One cannot step twice in the same river.",
+"Heraclitus",
+
+"If ignorance is bliss, there should be more happy people.",
+"Victor Cousin",
+
+"Only one man ever understood me, and he didn’t understand me.",
+"G. W. F. Hegel",
+
+"Once upon a time, I dreamt I was a butterfly, fluttering hither and thither, to all intents and purposes a butterfly. I was conscious only of my happiness as a butterfly, unaware that I was myself. Soon I awoke, and there I was, veritably myself again. Now I do not know whether I was then a man dreaming I was a butterfly, or whether I am now a butterfly, dreaming I am a man.",
+"Zhuangzi",
+
+"We live in the best of all possible worlds.",
+"Gottfried Wilhelm Leibniz",
+
+"If someone is able to show me that what I think or do is not right, I will happily change, for I seek the truth, by which no one was ever truly harmed. It is the person who continues in his self-deception and ignorance who is harmed.",
+"Marcus Aurelius",
+
+"The mind is furnished with ideas by experience alone.",
+"John Locke",
+
+"Doing nothing is better than being busy doing nothing.",
+"Lao Tzu",
+
+"It is undesirable to believe a proposition when there is no ground whatever for supposing it true.",
+"Bertrand Russell",
+
+"Physics isn’t the most important thing. Love is.",
+"Richard P. Feynman",
+
+"The unexamined life is not worth living.",
+"Socrates",
+
+"I never found beauty in longing for the impossible and never found the possible to be beyond my reach.",
+"Ayn Rand",
+
+"I think therefore I am.",
+"René Descartes",
+
+"I assess the power of a will by how much resistance, pain, torture it endures and knows how to turn to its advantage",
+"Fredrich Nietzsche",
+
+"Metaphysics is a dark ocean without shores or lighthouse, strewn with many a philosophic wreck.",
+"Immanuel Kant",
+
+"Only that day dawns to which we are awake. There is more day to dawn. The sun is but a morning star.",
+"Henry David Thoreau",
+
+"Virtue is nothing else than right reason.",
+"Seneca",
+
+"The energy of the mind is the essence of life.",
+"Aristotle",
+
+"Man is the measure of all things.",
+"Protagoras",
+
+"Madness is something rare in individuals — but in groups, parties, peoples, and ages, it is the rule.",
+"Friedrich Nietzsche",
+
+"In everything, there is a share of everything.",
+"Anaxagoras",
+
+"The more you struggle to live, the less you live. Give up the notion that you must be sure of what you are doing. Instead, surrender to what is real within you, for that alone is sure….you are above everything distressing.",
+"Spinoza",
+
+"No man’s knowledge here can go beyond his experience.",
+"John Locke",
+
+"Seize the moments of happiness, love and be loved! That is the only reality in the world, all else is folly. It is the one thing we are interested in here.",
+"Leo Tolstoy",
+
+"Entities should not be multiplied unnecessarily.",
+"William of Ockham",
+
+"One’s life has value so long as one attributes value to the life of others, by means of love, friendship, and compassion",
+"Simone de Beauvoir",
+
+"Philosophy is a battle against the bewitchment of our intelligence by means of language.",
+"Ludwig Wittgenstein",
+]
+
+const quoteChanger = color => {
+  const quoteBox = document.querySelector('#quote-box')
+  const quote = document.querySelector('#quote')
+  const quoteAuthor = document.querySelector('#quote-author')
+
+  quoteBox.classList.remove(quoteBox.classList[1])
+  quoteBox.classList.remove(quoteBox.classList[1])
+  quoteBox.classList.add(color)
+  
+  const rand = Math.floor(Math.random() * QUOTE.length/2) * 2
+  quote.textContent = QUOTE[rand]
+  quoteAuthor.textContent = QUOTE[rand+1]
+}
+
+const heroConverter = () => {
+  const heroConverterInput = document.querySelector('#converter-input')
+  const heroConverterOption = document.querySelector('#converter-option')
+  const heroConverterResult = document.querySelector('#converter-result')
+
+  const value = heroConverterInput.value
+  const option = heroConverterOption.value
+
+  if (option === 'lb_kg') {
+    heroConverterResult.textContent = Math.round(value * 0.45359237 * 100) / 100
+  } else if (option === 'kg_lb') {
+    heroConverterResult.textContent = Math.round(value / 0.45359237 * 100) / 100
+  }
+}
+
+const seriesProcessor = () => {
+  const seriesInput = document.querySelector('#series-input')
+
+  const seriesMax = document.querySelector('#series-max')
+  const seriesMin = document.querySelector('#series-min')
+  const seriesSum = document.querySelector('#series-sum')
+  const seriesAvg = document.querySelector('#series-avg')
+  const seriesRev = document.querySelector('#series-rev')
+
+  const seriesValues = seriesInput.value.split(/[\r\n\s,]+/)
+  const seriesNumbers = seriesValues.filter(n => n).map(n => Number(n))
+  const seriesReverse = seriesNumbers.reverse()
+  const seriesLength = seriesNumbers.length
+
+  let sum = 0
+
+  seriesNumbers.map(n => (sum += n))
+  console.log(seriesNumbers)
+
+  seriesMax.textContent = Math.max(...seriesNumbers)
+  seriesMin.textContent = Math.min(...seriesNumbers)
+  seriesSum.textContent = sum
+  seriesAvg.textContent = sum / seriesLength
+  seriesRev.textContent = seriesReverse.join(',')
+}
+
+const magicClear = () => {
+  const magicBox = document.querySelector('#magic-box')
+
+  magicBox.value = ''
+}
+const magicCapitalize = () => {
+  const magicBox = document.querySelector('#magic-box')
+  const caps = magicBox.dataset.caps
+
+  if (caps === 'lower') {
+    magicBox.value = magicBox.value.toLowerCase()
+    magicBox.dataset.caps = 'upper'
+  } else {
+    magicBox.value = magicBox.value.toUpperCase()
+    magicBox.dataset.caps = 'lower'
+  }
+}
+const magicStrip = () => {
+  const magicBox = document.querySelector('#magic-box')
+  let lines = magicBox.value.split(/\r?\n/)
+  lines = lines.filter(n => n).map(n => n.trim())
+
+  magicBox.value = lines.join('\n')
+}
+const magicSort = () => {
+  const magicBox = document.querySelector('#magic-box')
+  let lines = magicBox.value.split(/\r?\n/)
+  lines = lines.sort()
+
+  magicBox.value = lines.join('\n')
+}
+const magicReverse = () => {
+  const magicBox = document.querySelector('#magic-box')
+  let lines = magicBox.value.split(/\r?\n/)
+  lines = lines.reverse()
+
+  magicBox.value = lines.join('\n')
+}
+const magicAdd = () => {
+  const magicBox = document.querySelector('#magic-box')
+  let lines = magicBox.value.split(/\r?\n/)
+
+  lines = lines.map((n, index) => `${index + 1}. ${n}`)
+
+  magicBox.value = lines.join('\n')
+}
+const magicShuffle = () => {
+  const magicBox = document.querySelector('#magic-box')
+  const lines = magicBox.value.split(/\r?\n/)
+
+  magicBox.value = lines
+    .map(value => ({ value, sort: Math.random() }))
+    .sort((a, b) => a.sort - b.sort)
+    .map(({ value }) => value)
+    .join('\n')
+}
